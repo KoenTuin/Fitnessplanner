38c3086ea57ef5c161d5124020df08d46938d813
KoenTuin <33253619+KoenTuin@users.noreply.github.com>
2018-10-04 15:54:46 +0200

    FXML changes
    I've added a button to fill the labels, where the name of the excersises have to come.
    
    Right now the button is in the same FXML file as the labels, but it is supposed to be on the home screen.
    
    At this moment the button wil fill a label with data from a variable in the Controller. The data in this variable is supposed to be linked to a database.

--- Fitnessplanner/src/sample/Controller.java
+++ Fitnessplanner/src/sample/Controller.java
@@ -1,4 +1,14 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
 public class Controller {
+    public Label textLabel;
+    public String inhoud;
+
+    public void button(ActionEvent actionEvent){
+        inhoud = "Test oefening 1";
+        textLabel.setText(inhoud);
+    }
 }
--- Fitnessplanner/src/sample/Main.java
+++ Fitnessplanner/src/sample/Main.java
@@ -10,7 +10,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("basicHome.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("Menu.fxml"));
         primaryStage.setTitle("Fitness applicatie");
         primaryStage.setScene(new Scene(root));
         primaryStage.setMaximized(true);
