754c3aa4b29b21052eb294da17bf55bff084ead2
KoenTuin <33253619+KoenTuin@users.noreply.github.com>
2018-10-11 17:01:54 +0200

    Functions

--- Fitnessplanner/src/fitnessplanner/controllers/Controller.java
+++ Fitnessplanner/src/fitnessplanner/controllers/Controller.java
@@ -1,29 +1,69 @@
 package fitnessplanner.controllers;
 
+import fitnessplanner.models.MyJDBC;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TitledPane;
+import javafx.fxml.Initializable;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
 
-public class Controller {
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
     public Label textLabel;
     public String inhoud;
     public Button showText;
     public TextArea discription;
     public TitledPane buik;
+    public VBox buikExcercises;
 
-    public void button(ActionEvent actionEvent){
+    public void button(ActionEvent actionEvent) {
         inhoud = "Test oefening 1";
         textLabel.setText(inhoud);
 
     }
+
     @FXML
-    public void showText(){
+    public void showExcercises() throws SQLException {
         System.out.println("It works");
         //Get from database data which is from catagory buik.gettext
-        discription.setText("It works");
-        System.out.println(buik.getText());
+
+
+    }
+
+    @FXML
+    public void showDescription() throws SQLException {
+        MyJDBC db = new MyJDBC();
+        ResultSet resultSet = db.executeResultSetQuery("SELECT description FROM workout");
+
+        while (resultSet.next()) {
+            discription.appendText(resultSet.getString("description") + "\n");
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        discription.setText("");
+        MyJDBC db = new MyJDBC();
+        ResultSet resultSet = null;
+        try {
+            resultSet = db.executeResultSetQuery("SELECT name FROM workout");
+
+            while (resultSet.next()) {
+                String naam = resultSet.getString("name");
+                Label label = new Label(naam);
+//                label.setId(naam);
+//                Fix dat de labels clickable worden en dat de discription van de des betreffende oefening wordt
+//                weergegeven.
+//                label.setOnMouseClicked();
+                buikExcercises.getChildren().add(label);
+
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
