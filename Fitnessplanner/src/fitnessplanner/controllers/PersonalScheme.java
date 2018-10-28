a2eafa29582d866ee7855b2794f09e92d1a71d4d
YouriPel <youri.pellicaan@hva.nl>
2018-10-28 20:30:53 +0100

    personalScheme screen fix

--- Fitnessplanner/src/fitnessplanner/controllers/PersonalScheme.java
+++ Fitnessplanner/src/fitnessplanner/controllers/PersonalScheme.java
@@ -2,11 +2,48 @@ package fitnessplanner.controllers;
 
 import javafx.fxml.FXML;
 
-import javax.swing.text.html.ListView;
+import javax.swing.text.TableView;
 
 public class PersonalScheme {
     @FXML
-    private ListView schemeListView;
+    private TableView schemeListView;
+
+
+    public void setTableViewData(){
+//        while (resultSet.next()) {
+//            Vhousenumber = resultSet.getString("Vhousenumber");
+//            Vcity = resultSet.getString("Vcity");
+//            Vzipcode = resultSet.getString("Vzipcode");
+//            Hotelname = resultSet.getString("Hotelname");
+
+            //if currentdate -aantaldagen <= to database date than show
+//            if (date.compareTo(datadb) <= 0 && returnBagage == isGevonden) {
+//                bagagetabel.add(new bagageTabel(BagageID, Date, Time, State,
+//                        Labelnumber, Type, Brand, Color1, Color2, Characteristics,
+//                        IsReturned, Airport, Location, Initial, Insertion, Surname,
+//                        Street, Housenumber, Zipcode, City, Email, Phone1, Phone2,
+//                        Flightnumber, From, To, Vstreet, Vhousenumber, Vcity, Vzipcode,
+//                        Hotelname));
+
+//                for (int i = 0; i < table.getColumns().size(); i++) {
+//                    TableColumn column = (TableColumn) table.getColumns().get(i);
+//                    column.setCellValueFactory(new PropertyValueFactory(column.getId()));
+//                }
+//            }
+//        }
+//
+//        table.setItems(bagagetabel);
+    }
+    
+
+    @FXML
+    public void deleteExercise(){
+
+    }
+    @FXML
+    public void showExercises(){
+
+    }
 
 
 
