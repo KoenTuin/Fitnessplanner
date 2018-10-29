4cb08fe4a2a22901d35327a4e9a185f699058296
KoenTuin <33253619+KoenTuin@users.noreply.github.com>
2018-10-29 22:50:14 +0100

    delete button fixed
    now deletes exercise from list and refreshes scene

--- Fitnessplanner/src/fitnessplanner/controllers/PersonalScheme.java
+++ Fitnessplanner/src/fitnessplanner/controllers/PersonalScheme.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -17,7 +18,9 @@ import models.PersonalExercisesList;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+
 
 public class PersonalScheme implements Initializable {
     @FXML
@@ -25,12 +28,9 @@ public class PersonalScheme implements Initializable {
 
     @FXML
     private HBox setTable;
-
     @FXML
     private AnchorPane personalScreen;
 
-    @FXML
-    private Label statusLabel;
 
     PersonalExercisesList personalListOfExercises = PersonalExercisesList.getPersonalExerciselists();
 
@@ -38,7 +38,7 @@ public class PersonalScheme implements Initializable {
 
 
     @FXML
-    public void schemeTableView(){
+    public void schemeTableView() {
 
         TableColumn<Exercises, String> nameColumn = new TableColumn<>("Name");
         nameColumn.setMinWidth(150);
@@ -48,35 +48,45 @@ public class PersonalScheme implements Initializable {
         descriptionColumn.setMinWidth(400);
         descriptionColumn.setCellValueFactory(new PropertyValueFactory<>("description"));
 
-        schemeTableView = new TableView<>();
+        if (schemeTableView == null) {
+            schemeTableView = new TableView<>();
+        }
         schemeTableView.setItems(personalExercises);
         schemeTableView.getColumns().addAll(nameColumn, descriptionColumn);
 
         setTable.getChildren().addAll(schemeTableView);
 
     }
-    
+
 
     @FXML
-    public void deleteExercise(ActionEvent event){
-        ObservableList<Exercises> exerciseSelected, allExercises;
-        allExercises = schemeTableView.getItems();
+    public void deleteExercise(ActionEvent event) throws IOException {
+        ObservableList<Exercises> exerciseSelected;
         exerciseSelected = schemeTableView.getSelectionModel().getSelectedItems();
-
-        exerciseSelected.forEach(allExercises::remove);
+        String selectedExercise = exerciseSelected.toString().replaceAll("\\s+", "").replaceAll("\\[", "").replaceAll("\\]", "");
+        for (Exercises e : personalListOfExercises.PersonalListOfExercises) {
+            if (e.toString().replaceAll("\\s+", "").equals(selectedExercise)) {
+                personalListOfExercises.PersonalListOfExercises.remove(e);
+                break;
+            }
+        }
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("/views/personalScheme.fxml"));
+        personalScreen.getChildren().setAll();
+        personalScreen.getChildren().setAll(pane);
 
     }
+
     @FXML
-    public void showExercises(ActionEvent event) throws IOException{
-            //laad de nieuwe table in de bestaande anchorpane
-            AnchorPane pane = FXMLLoader.load(getClass().getResource("/views/menu.fxml"));
-            //maakt de oude table leeg
-            personalScreen.getChildren().setAll();
-            //laad de nieuwe table in
-            personalScreen.getChildren().setAll(pane);
-            //geeft de nieuwe table de juiste groote
-            pane.prefWidthProperty().bind(personalScreen.widthProperty());
-            pane.prefHeightProperty().bind(personalScreen.heightProperty());
+    public void showExercises(ActionEvent event) throws IOException {
+        //laad de nieuwe table in de bestaande anchorpane
+        AnchorPane pane = FXMLLoader.load(getClass().getResource("/views/menu.fxml"));
+        //maakt de oude table leeg
+        personalScreen.getChildren().setAll();
+        //laad de nieuwe table in
+        personalScreen.getChildren().setAll(pane);
+        //geeft de nieuwe table de juiste groote
+        pane.prefWidthProperty().bind(personalScreen.widthProperty());
+        pane.prefHeightProperty().bind(personalScreen.heightProperty());
     }
 
 
